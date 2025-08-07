ae3f88c127de17249284b56a5895e1fb17443da3
Mengxiao Lin <mengxiao.lin@liquid.ai>
2025-08-07 13:39:40 -0700

    Add RecipeGenerator for iOS

---
+++ iOS/RecipeGenerator/RecipeGenerator/ContentView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = GeneratorViewModel()
+    var body: some View {
+        VStack {
+            if (viewModel.isModelLoading) {
+                Text("Loading model")
+            } else if (viewModel.isGenerating) {
+                Text("Generating recipe...")
+            }else {
+                Button("Generate a new recipe") {
+                    Task {
+                        await viewModel.setupModel()
+                        do {
+                            try await viewModel.generateRecipe()
+                        } catch {
+                            print("Error: \(error)")
+                        }
+                    }
+                }
+            }
+            if let recipe = viewModel.recipe {
+                Text("Title: \(recipe.name)")
+                Text("Cooking time: \(recipe.cookingTime) minutes")
+                Text("Directions: \(recipe.directions.joined(separator: "\n"))")
+            
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/RecipeGenerator/RecipeGenerator/Models/Recipe.swift
@@ -0,0 +1,16 @@
+import Foundation
+import LeapSDK
+
+@Generatable("A recipe for cooking great dishes")
+struct Recipe: Codable {
+    @Guide("Name of the dish")
+    var name: String
+    @Guide("Time to cook the dish in minutes")
+    var cookingTime: Int
+    @Guide("Whether the dish is vegetarian or not")
+    var isVegetarian: Bool
+    @Guide("Ingredients needed")
+    var ingredients: [String]
+    @Guide("Directions of cooking")
+    var directions: [String]
+}
